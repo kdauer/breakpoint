733133a74b3da18e75ac292735818e602f7dc040
Konstantin <dauer.konstantin@gmail.com>
2020-02-11 15:10:43 +0100

    route to spotdetail

--- routes/spots.js
+++ routes/spots.js
@@ -4,6 +4,7 @@ const Spot = require("../models/Spot");
 
 router.get("/spotlist", (req, res) => {
   Spot.find({ type: req.query.breaktypes }).then(data => {
+    // console.log(data);
     let arr = data;
     let regions = [...new Set(arr.map(e => e.region))];
     var obj = {};
@@ -11,12 +12,24 @@ router.get("/spotlist", (req, res) => {
       return (obj[region] = []);
     });
     let regionName = arr.map(item => {
-      return obj[item.region].push(item.name);
+      return obj[item.region].push({
+        name: item.name,
+        id: item.id,
+        objId: item._id,
+        type: item.type
+      });
     });
     let sortedData = [];
     sortedData.push(obj);
-    console.log(obj);
+    // console.log(obj);
     res.render("spotlist.hbs", { obj });
+  });
+});
+
+router.get("/spotlist/detail/:id", (req, res) => {
+  Spot.find({ id: req.params.id }).then(info => {
+    console.log("here", info);
+    res.render("detail.hbs", info);
   });
 });
 
