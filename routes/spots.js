6146f415b3d73e83032bca1d030a3932cbb78f6f
Konstantin <dauer.konstantin@gmail.com>
2020-02-11 16:59:51 +0100

    added functions

--- routes/spots.js
+++ routes/spots.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const Spot = require("../models/Spot");
 
 router.get("/spotlist", (req, res) => {
@@ -26,10 +27,28 @@ router.get("/spotlist", (req, res) => {
   });
 });
 
+const funcs = require("../lib/utils");
+
 router.get("/spotlist/detail/:id", (req, res) => {
   Spot.find({ id: req.params.id }).then(info => {
-    console.log("here", info);
-    res.render("detail.hbs", { info });
+    // console.log("here", info);
+    return axios
+      .get(
+        `https://services.surfline.com/kbyg/spots/reports?spotId=${req.params.id}`
+      )
+      .then(responseFromAPI => {
+        let overview = responseFromAPI.data.forecast;
+        const windInfo = funcs.wind(overview.wind);
+        const tideTime = funcs.convertTSnext(overview.tide);
+        const nextTideInfo = funcs.nextTide(overview.tide);
+        // res.json(responseFromAPI.data.forecast);
+        res.render("detail.hbs", {
+          overview,
+          windInfo,
+          tideTime,
+          nextTideInfo
+        });
+      });
   });
 });
 
