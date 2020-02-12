5eedbd7095d041a0c1080164696f81566ab3200c
Konstantin <dauer.konstantin@gmail.com>
2020-02-12 12:48:05 +0100

    lets merge this

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost:27017/Break-Point", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Break-Point", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
