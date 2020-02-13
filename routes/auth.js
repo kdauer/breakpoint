468acfdffa213b95050987d0eb138b7cc60b3411
borndead <shaulitto1@gmail.com>
2020-02-13 20:19:33 +0100

    stupid fav spots working

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
-const Spots = require("./spots");
+const Spots = require("../models/Spot");
 
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
@@ -69,38 +69,40 @@ router.get("/logout", (req, res) => {
 });
 
 router.post("/auth/user/favourites/:id", (req, res, next) => {
-  // console.log(req.params);
-  User.findById(req.user._id)
-    .then(user => {
-      if (!user.favourites.includes(req.params.id)) {
-        User.findByIdAndUpdate(
-          req.user._id,
-          {
-            $push: { favourites: req.params.id }
-          },
-          { new: true }
-        )
-          .populate({ path: "favourites" })
-          .then(result => {
-            res.json();
-            // res.render("auth/user.hbs", { result: result });
-          })
+  console.log(req.params);
+  Spots.findOne({ id: req.params.id }).then(spotInfo => {
+    User.findById(req.user._id)
+      .then(user => {
+        if (!user.favourites.includes(spotInfo._id)) {
+          User.findByIdAndUpdate(
+            req.user._id,
+            {
+              $push: { favourites: spotInfo._id }
+            },
+            { new: true }
+          )
+            .populate({ path: "favourites" })
+            .then(result => {
+              res.json();
+              // res.render("auth/user.hbs", { result: result });
+            })
 
-          .catch(err => {
-            next(err);
-          });
-      } else {
-        res.json();
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+            .catch(err => {
+              next(err);
+            });
+        } else {
+          res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 });
 
 router.get("/auth/user", (req, res) => {
   User.findById(req.user._id)
-    .populate("favourites")
+    .populate({ path: "favourites" })
     .then(result => {
       console.log("here are the results", result);
       res.render("auth/user.hbs", { result: result });
