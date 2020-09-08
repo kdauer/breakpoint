f32aa8ae2dd59f97ed0cc6c00ebdc1a9dae45725
Konstantin <dauer.konstantin@gmail.com>
2020-09-08 11:44:39 +0200

    migrate to mongodb atlas

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
-const passport = require("passport");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
 
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const flash = require("connect-flash");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Break-Point", {
+  .connect(process.env.DB_URI || 'mongodb://localhost:27017/Break-Point', {
     useNewUrlParser: true
   })
   .then(x => {
@@ -24,18 +24,18 @@ mongoose
     );
   })
   .catch(err => {
-    console.error("Error connecting to mongo", err);
+    console.error('Error connecting to mongo', err);
   });
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
+const app_name = require('./package.json').name;
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
 );
 
 const app = express();
 
 // Middleware Setup
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -43,24 +43,24 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
     sourceMap: true
   })
 );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = "Break-Point-App";
+app.locals.title = 'Break-Point-App';
 
 app.use(
   session({
-    secret: "our-passport-local-strategy-app",
+    secret: 'our-passport-local-strategy-app',
     resave: true,
     saveUninitialized: true,
     store: new MongoStore({ mongooseConnection: mongoose.connection })
@@ -68,7 +68,7 @@ app.use(
 );
 
 app.use(flash());
-require("./Passport/index")(app);
+require('./Passport/index')(app);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -77,13 +77,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
 
-const spotRoutes = require("./routes/spots");
-app.use("/", spotRoutes);
+const spotRoutes = require('./routes/spots');
+app.use('/', spotRoutes);
 
-const authRoute = require("./routes/auth");
-app.use("/", authRoute);
+const authRoute = require('./routes/auth');
+app.use('/', authRoute);
 
 module.exports = app;
